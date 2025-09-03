e0ccb79243d816be02952ca958fd5aa0da5a30f9
Karl Joshua Encio <enciokarl@gmail.com>
2025-09-03 13:45:18 +0800

    Day 23 Finished

---
+++ day23/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  alias  = "bootstrap"
+  region = "ap-southeast-1"
+}
+
+data "aws_secretsmanager_secret" "creds" {
+  provider = aws.bootstrap
+  name     = "terraform/aws-credentials"
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+  provider  = aws.bootstrap
+  secret_id = data.aws_secretsmanager_secret.creds.id
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string).access_key
+  secret_key = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string).secret_key
+}
+
+module "vpc" {
+  source            = "./modules/vpc"
+  vpc_cidr          = var.vpc_cidr
+  public_subnet     = var.public_subnet
+  private_subnet    = var.private_subnet
+  availability_zone = var.availability_zone
+}
